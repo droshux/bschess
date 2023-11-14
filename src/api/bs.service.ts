b157ace515b9bc1907f71f59339272c0a1d9212d
Hux <droshux@gmail.com>
2023-11-14 14:14:13 +0000

    Created CRUD services and changed model slightly.
    BSs is now indexed by ID ints, IDs are sequential
    and not-re-used when deleted.

---
+++ src/api/bs.service.ts
@@ -0,0 +1,47 @@
+import { BS } from "../models/bs"
+import { regularPiece,  } from "../piececost"
+
+let BSs: {[id: number]: BS} = {
+  1: {
+    name: "Test Event",
+    lore: "Test Lore",
+    setup: {
+      cost: regularPiece("pawn"),
+      extra: ""
+    },
+    effect: "Test Effect"
+  },
+  2: {
+    name: "Test Piece",
+    lore: "Test Lore 2",
+    setup: {
+      cost: regularPiece("pawn"),
+      extra: ""
+    },
+    effect: "Test Effect 2",
+    move: "Like a pawn",
+    take: "[move]",
+    lives: 1
+  }
+}
+
+export const create = async (nbs: BS): Promise<BS> => {
+  // One more than the largest ID (⌈/⍕¨)Object.keys(BSs)
+  const newID: number = Object.keys(BSs)
+    .map((a: string) => +a) 
+    .reduce((a: number, b: number) => Math.max(a, b))+1
+
+  BSs[newID] = nbs
+  return BSs[newID]
+}
+export const findall = async (): Promise<BS[]> => Object.values(BSs)
+export const find = async (id: number): Promise<BS> => BSs[id]
+export const update = async (id: number, ubs: BS) => {
+  if (!await find(id)) return null
+  BSs[id] = ubs;
+  return BSs[id];
+}
+export const remove = async (id: number): Promise<null | void> => {
+  if (!await find(id)) return null
+  delete BSs[id]
+}
--- src/api/bs.ts
+++ src/api/bs.ts
@@ -8,15 +8,17 @@ export interface Base_BS {
   setup: Setup // The setup required to activate the BS
   effect: string // What does the BS do?
 }
-export interface BS extends Base_BS {id: number}
 
 export interface Setup {
   cost: PieceCost, // The pieces that must be used in it's construction
   extra: string // Any other setup requirements eg: "wait 2 turns"
 }
 
-export interface Piece_BS {
+export interface Piece_BS extends Base_BS {
   move: string // eg: "Like a rook but can jump over pieces"
   take: string // eg: [moves] or like a bishop
   lives: number // eg: 2 (1 is the default) 
 }
+
+export type BS = Base_BS | Piece_BS
+// export type BS = Partial<Piece_BS>
